a1ae61ffbc057803c70c4b51247570dd03f6a129
BirkurwarJay <birkurwarjay01@gmail.com>
2024-07-24 18:41:43 +0530

    add component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Lenditt'`, () => {
+  it(`should have as title 'Home | Lenditt-India'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Lenditt');
+    expect(app.title).toEqual('Home | Lenditt-India');
   });
 
   it('should render title', () => {
